bca0b9391136c013518a9eeaae14266a46e86774
rahmahamdy590-del <rahmahamdy590@gmail.com>
2026-07-31 03:02:43 +0300

    Update 00_initialize_database.sql

--- script/00_initialize_database.sql
+++ script/00_initialize_database.sql
@@ -1,14 +1,20 @@
-CREATE DATABASE HR_Project;
+IF DB_ID('HR_Project') IS NULL
+BEGIN
+    CREATE DATABASE HR_Project;
+END
 GO
 
 USE HR_Project;
 GO
 
-CREATE SCHEMA bronze;
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'bronze')
+    EXEC('CREATE SCHEMA bronze');
 GO
 
-CREATE SCHEMA silver;
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'silver')
+    EXEC('CREATE SCHEMA silver');
 GO
 
-CREATE SCHEMA gold;
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'gold')
+    EXEC('CREATE SCHEMA gold');
 GO
